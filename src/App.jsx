3589c03ef6044d21163e57d77852a61898d90ba2
Mahroof vp <mahroofvp77@gmail.com>
2023-11-29 21:18:50 +0530

    completed the first layer of the project (Home section)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Home} from './components/index'
+
+const App = () => {
+  return (
+   <>
+    <main>
+      <Home/>
+    </main>
+   </>
+  )
+}
+
+export default App
